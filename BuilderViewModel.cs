a71d34a4b7689d86bed33917df3559d65d3b599d
karalius32 <juozapasjurksa@gmail.com>
2025-03-02 00:20:27 +0200

    TASK: paprasta drobė

--- BuilderViewModel.cs
+++ BuilderViewModel.cs
@@ -1,18 +1,21 @@
 ﻿using InteractiveNeuralNetworks.Commands;
+using InteractiveNeuralNetworks.Controls;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace InteractiveNeuralNetworks
 {
     internal class BuilderViewModel : ViewModelBase
     {
         public BuilderModel Model { get; set; }
+        public CanvasViewModel CanvasViewModel { get; set; }
 
+        // Hello world
         public RelayCommand ClickMeButtonCommand { get; set; }
-
         public int Counter
         {
             get => Model.ClickCounter;
@@ -22,10 +25,11 @@ namespace InteractiveNeuralNetworks
                 OnPropertyChanged(nameof(Counter));
             }
         }
-
+ 
         public BuilderViewModel()
         {
             Model = new BuilderModel();
+            CanvasViewModel = new CanvasViewModel();
 
             ClickMeButtonCommand = new RelayCommand(ExecuteClickMe, CanExecuteClickMe);
         }
@@ -34,6 +38,8 @@ namespace InteractiveNeuralNetworks
         private void ExecuteClickMe(object obj)
         {
             Counter++;
+            //CanvasViewModel.CanvasItems.Add(new CanvasItemViewModel(50, 50, "Black"));
+
         }
         private bool CanExecuteClickMe(object obj)
         {
---
+++ Controls/CanvasItemViewModel.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Navigation;
+
+namespace InteractiveNeuralNetworks.Controls
+{
+    internal class CanvasItemViewModel : ViewModelBase
+    {
+        private bool _IsSelected;
+        public bool IsSelected
+        {
+            get => _IsSelected;
+            set
+            {
+                _IsSelected = value;
+                if (IsSelected) Border = 1;
+                else Border = 0;
+                OnPropertyChanged(nameof(IsSelected));
+            }
+        }
+
+        public System.Windows.Point SelectionPosition { get; set; }
+
+        private int _Border;
+        public int Border
+        {
+            get => _Border;
+            set
+            {
+                _Border = value;
+                OnPropertyChanged(nameof(Border));
+            }
+        }
+
+
+        private System.Windows.Point _Position;
+        public System.Windows.Point Position
+        {
+            get => _Position;
+            set
+            {
+                _Position = value;
+                OnPropertyChanged(nameof(Position));
+            }
+        }
+
+        private System.Windows.Point _StablePosition;
+        public System.Windows.Point StablePosition
+        {
+            get => _StablePosition;
+            set
+            {
+                _StablePosition = value;
+                OnPropertyChanged(nameof(StablePosition));
+            }
+        }
+
+        private string _Color;
+        public string Color
+        {
+            get => _Color;
+            set
+            {
+                _Color = value;
+                OnPropertyChanged(nameof(Color));
+            }
+        }
+
+        private int _Width;
+        public int Width
+        {
+            get => _Width;
+            set
+            {
+                _Width = value;
+                OnPropertyChanged(nameof(Width));
+            }
+        }
+
+        private int _Height;
+        public int Height
+        {
+            get => _Height;
+            set
+            {
+                _Height = value;
+                OnPropertyChanged(nameof(Height));
+            }
+        }
+
+        public CanvasItemViewModel(double x, double y, int width, int height, string color)
+        {
+            Position = new System.Windows.Point(x, y);
+            StablePosition = new System.Windows.Point(x, y);
+            Color = color;
+            Width = width;
+            Height = height;
+        }
+
+        public void CheckIfSelected(MouseEventArgs e)
+        {
+            System.Windows.Point mousePos = e.GetPosition((IInputElement)e.Source);
+            System.Windows.Point relativeMousePos = e.GetPosition((IInputElement)e.OriginalSource);
+            if (mousePos.X >= StablePosition.X && mousePos.X <= StablePosition.X + Width &&
+                mousePos.Y >= StablePosition.Y && mousePos.Y <= StablePosition.Y + Height)
+            {
+                IsSelected = true;
+                SelectionPosition = relativeMousePos;
+            }
+        }
+    }
+}
