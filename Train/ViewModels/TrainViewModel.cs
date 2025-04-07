1a39a4e9edb2272ed4f0beb5b7fcec3dd64da288
Ignas Nakčeris <ignasnak@gmail.com>
2025-04-08 00:55:48 +0300

    button

--- Train/ViewModels/TrainViewModel.cs
+++ Train/ViewModels/TrainViewModel.cs
@@ -13,6 +13,11 @@ using System.Windows.Controls;
 using Train.Helpers;
 
 
+using Shared.Commands;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Threading;
 
 namespace Train.ViewModels
 {
@@ -57,12 +62,38 @@ namespace Train.ViewModels
         public HyperparametersWindowViewModel HyperparametersWindowViewModel { get; set; }
 
         public RelayCommand GetDirectoryButtonCommand { get; set; }
+        private string _outputText = "";
+        [JsonIgnore]
+        public string OutputText
+        {
+            get => _outputText;
+            set 
+            { 
+                _outputText = value; 
+                OnPropertyChanged(nameof(OutputText)); 
+            }
+        }
+
+        [JsonIgnore]
+        public ICommand RefreshLogCommand { get; }
+
+        private FileSystemWatcher _logFileWatcher;
+        private string _logFilePath;
+        private DispatcherTimer _refreshTimer;
+        private DateTime _lastReadTime = DateTime.MinValue;
 
         public TrainViewModel()
         {
             InitializeHyperparameters();
             HyperparametersWindowViewModel = new HyperparametersWindowViewModel(this);
             GetDirectoryButtonCommand = new RelayCommand(ExecuteClickMe, CanExecuteClickMe);
+
+
+            string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            _logFilePath = Path.Combine(baseDirectory, "../../../../Python-server/log.txt");
+            InitializeWatcher();
+            RefreshLogCommand = new RelayCommand(_ => RefreshLogContent());
+            
         }
 
         private void InitializeHyperparameters()
@@ -87,5 +118,60 @@ namespace Train.ViewModels
         {
             return true;
         }
+
+        private void InitializeWatcher()
+        {
+            _refreshTimer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromSeconds(1)
+            };
+            _refreshTimer.Tick += (s, e) => RefreshLogContent();
+            _refreshTimer.Start();
+
+            if (File.Exists(_logFilePath))
+            {
+                string directory = Path.GetDirectoryName(_logFilePath);
+                string filename = Path.GetFileName(_logFilePath);
+
+                _logFileWatcher = new FileSystemWatcher(directory, filename)
+                {
+                    EnableRaisingEvents = true,
+                    NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.Size
+                };
+                _logFileWatcher.Changed += (s, e) => System.Windows.Application.Current.Dispatcher.Invoke(RefreshLogContent);
+
+            }
+            else
+                OutputText = "The log file does not exist: " + _logFilePath;
+
+            RefreshLogContent();
+        }
+
+
+        public async void RefreshLogContent()
+        {
+            try
+            {
+                var fileInfo = new FileInfo(_logFilePath);
+                if (!fileInfo.Exists || fileInfo.LastWriteTime <= _lastReadTime)
+                    return;
+
+                _lastReadTime = fileInfo.LastWriteTime;
+
+                using (var fs = new FileStream(_logFilePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                using (var reader = new StreamReader(fs, Encoding.UTF8))
+                {
+                    OutputText = await reader.ReadToEndAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                OutputText = $"Error reading log file: {ex.Message}";
+            }
+        }
+
+
+	
+
     }
 }
